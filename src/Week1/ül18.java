1ac94580600d42f13b59632bd58e6785f9be0c26
Anna-Liisa Saks <annaliisa.saks@gmail.com>
2021-09-24 20:10:39 +0300

    Fixes task 18

--- src/Week1/ül18.java
+++ src/Week1/ül18.java
@@ -12,17 +12,17 @@ public class ül18 {
 
         if(0>typedPoints || typedPoints>60) {
             grade="-";
-        } else if (0<=typedPoints && typedPoints<30) {
+        } else if (typedPoints<30) {
             grade = "failed";
-        } else if (30<=typedPoints && typedPoints<35) {
+        } else if (typedPoints<35) {
             grade="1";
-        } else if(35<=typedPoints && typedPoints<40) {
+        } else if(typedPoints<40) {
             grade="2";
-        }else if(40<=typedPoints && typedPoints<45) {
+        }else if(typedPoints<45) {
             grade="3";
-        }else if(45<=typedPoints && typedPoints<50) {
+        }else if( typedPoints<50) {
             grade="4";
-        }else if(50<=typedPoints && typedPoints<61) {
+        }else {
             grade="5";
         }
         System.out.println("Grade:  " + grade);
