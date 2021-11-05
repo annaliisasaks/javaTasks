c501999d017966f89971044cb3687db89331c103
Anna-Liisa Saks <annaliisa.saks@gmail.com>
2021-11-05 18:32:59 +0200

    Week 6

---
+++ src/Week6/yl97.java
@@ -0,0 +1,26 @@
+package Week6;
+
+public class yl97 {
+
+    public static void main(String[] args) {
+        // test method here
+        int[] array = {5, 1, 3, 4, 2};
+        printElegantly(array);
+    }
+
+    public static void printElegantly(int[] array) {
+        // separate with comma and whitespace, no comma trailing at end of last number.
+
+        for (int count = 0; count < array.length; count++) {
+
+            System.out.print(array[count]);
+
+            if (count == array.length-1) {
+                // do nothing at end
+            } else {
+                System.out.print(", ");
+            }
+        }
+
+    }
+}
